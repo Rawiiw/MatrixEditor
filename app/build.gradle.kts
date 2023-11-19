d7bdeb4bbf68b369dd53f73e792b99b962a0e65f
Alexandra <Alexandra@DESKTOP-62FJK6E>
2023-11-19 21:13:48 +0700

    доделать страницы с добавлением и редактированием

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.matrixeditor"
-    compileSdk = 33
-
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.matrixeditor"
@@ -35,14 +34,16 @@ android {
     }
 }
 
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment:2.5.3")
-    implementation("androidx.navigation:navigation-ui:2.5.3")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
-    implementation("com.google.firebase:firebase-inappmessaging:20.3.5")
+    implementation("androidx.navigation:navigation-fragment:2.7.5")
+    implementation("androidx.navigation:navigation-ui:2.7.5")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.firebase:firebase-inappmessaging:20.4.0")
+    implementation("androidx.compose.ui:ui-graphics-android:1.5.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
